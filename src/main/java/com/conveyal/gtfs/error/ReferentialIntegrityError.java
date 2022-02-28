771d62e96605ea718ba45ba0d24dfb0288ab90bc
MonsterDruide1 <5958456@gmail.com>
2022-02-28 20:54:12 +0100

    Add parameterless constructor to ReferentialIntegrityError

--- src/main/java/com/conveyal/gtfs/error/ReferentialIntegrityError.java
+++ src/main/java/com/conveyal/gtfs/error/ReferentialIntegrityError.java
@@ -9,6 +9,11 @@ public class ReferentialIntegrityError extends GTFSError implements Serializable
     // TODO: maybe also store the entity ID of the entity which contained the bad reference, in addition to the row number
     public final String badReference;
 
+    public ReferentialIntegrityError() {
+        super(null);
+        badReference = null;
+    }
+
     public ReferentialIntegrityError(String tableName, long row, String field, String badReference) {
         super(tableName, row, field);
         this.badReference = badReference;
